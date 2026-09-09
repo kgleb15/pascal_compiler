a350d281306f987b105389fa0128654354b31f18
Kulikov Gleb <kulgleb15@gmail.com>
2026-09-09 23:36:32 +0300

    feat: Переход на C++

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <FlexLexer.h>
+
+int main(int argc, char **argv) {
+    std::ifstream file;
+    std::istream *input = &std::cin;
+
+    if (argc > 1) {
+        file.open(argv[1]);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file: " << argv[1] << std::endl;
+            return 1;
+        }
+        input = &file;
+    }
+
+    yyFlexLexer lexer;
+    lexer.switch_streams(input, &std::cout); 
+    lexer.yylex();
+
+    return 0;
+}
