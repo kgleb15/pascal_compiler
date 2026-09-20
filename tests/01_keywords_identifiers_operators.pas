560123ff76ab57f50f435b639a1eb7b0e87d6a53
Kulikov Gleb <kulgleb15@gmail.com>
2026-09-20 19:59:26 +0300

    feat: Добавлены файлы с тестами на определенные темы

---
+++ tests/01_keywords_identifiers_operators.pas
@@ -0,0 +1,29 @@
+// Некоторые ключевые слова
+and in mod then xor as class inline try
+
+// Идентификаторы
+x y z index name read MyVar _private var123 CamelCase snake_case
+&if &begin &do &else
+
+// Некоторые операторы
+<< += <> >< <=
++ / < ] ; . ( ^
+
+
+Program Test;
+x = 42;
+&if x = 42 then
+begin
+    y := x + 1;
+    x := x - 1;
+end
+else
+begin
+    y := 0;
+    x += 1; x -= 1;
+    a := b << 2; e := f * 2;
+    &begin := 1; &do := 2; &else := &17;
+    type PInt = ^Integer;
+    p^.x := 1;
+    var index, name, read, write, &class, &else: integer;
+end;
---
+++ tests/04_string_constants.pas
@@ -0,0 +1,38 @@
+q := 'Hello, world!';
+r := '';
+s := 'a';
+
+
+t := 'it''s a test';
+u := '''';
+v := '''quoted''';
+
+
+w := #77;
+x := #13#10;
+pp := #$41;
+rr := #&101;
+ss := #%1000001;
+
+
+y := 'Tab:'#9'End';
+z := 'Line1'#13#10'Line2';
+zz := 'a'#62'b';
+
+
+zzz := 'a' #62 'b';
+
+// Caret-нотация
+kk := ^Z;
+kkk := ^ZZ; // >1 символа
+
+
+aa := 'contains // not a comment';
+bb := 'contains { not a comment }';
+cc := 'contains (* not a comment *)';
+dd := '42 and 3.14';
+
+
+ff := #300;
+gg := #abc;
+q  := 'abc
